729e0a20e93528a77e45cc6fcb5d47364d749c67
Warong Lupaiboon <warong@vaivaisoft.com>
2013-05-29 04:26:56 +0700

    Initializing seed array

--- blogberry/seed-custom/functions.php
+++ blogberry/seed-custom/functions.php
@@ -6,13 +6,6 @@
 
 	add_action( 'wp_enqueue_scripts', 'seed_scripts_method' ); // wp_enqueue_scripts action hook to link only on the front-end
 
-	function seed_admin_theme_style() {
-	    wp_enqueue_style('seed-admin-theme', get_template_directory_uri().'/seed-custom/css/admin.css');
-	}
-
-	add_action('admin_enqueue_scripts', 'seed_admin_theme_style');
-	add_action('login_enqueue_scripts', 'seed_admin_theme_style');
-
 	global $seed;
 	global $themename;
 	global $fontface_files;
@@ -21,6 +14,9 @@
 	$themename = preg_replace("/\W/", "_", strtolower($themename) );
 
 	$seed = get_option($themename);
+
+	if(!is_array($seed))
+		$seed = array();
 
 	$seed = $seed + array(
 											"logo_checkbox" => 1,
@@ -535,3 +531,10 @@
 
 		return $classes;	
 	}
+
+	function seed_admin_theme_style() {
+	    wp_enqueue_style('seed-admin-theme', get_template_directory_uri().'/seed-custom/css/admin.css');
+	}
+
+	add_action('admin_enqueue_scripts', 'seed_admin_theme_style');
+	add_action('login_enqueue_scripts', 'seed_admin_theme_style');
